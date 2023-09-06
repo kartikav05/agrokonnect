78590654e8155dad9b03f49bed72364dda0a1d0a
Kartik Avadhani <kartikav2001@gmail.com>
2023-09-06 14:03:45 +0530

    Fixed No. of Notifications and made it display acrosss all pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     if user_signed_in?
       id= current_user.id
       notifications = Notification.where(status: 'pending', user_id: id)
-      @unread= notifications.length
+      $unread= notifications.length
       end
   end
 end
