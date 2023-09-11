bda00a233f0de42fc8409f337a654e65cfae739c
Kartik Avadhani <kartikav2001@gmail.com>
2023-09-11 19:19:22 +0530

    Removed Comments,unwanted code and added code formatting

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,16 +1,15 @@
-import consumer from "./consumer"
+import consumer from "./consumer";
 
-const chatRoom = document.getElementById('chat-room')
+const chatRoom = document.getElementById("chat-room");
 if (chatRoom) {
-  const chatMessages = document.getElementById('my-chat')
- // const chatInput = document.getElementById('chat-text-input')
-  const chatForm = document.getElementById('chat-form')
-  const userId = Number(document.getElementById('chat-text-input2').value);
+  const chatMessages = document.getElementById("my-chat");
+  const chatForm = document.getElementById("chat-form");
+  const userId = Number(document.getElementById("chat-text-input2").value);
 
   consumer.subscriptions.create("RoomChannel", {
     connected() {
       // Called when the subscription is ready for use on the server
-      console.log('connected')
+      console.log("connected");
     },
 
     disconnected() {
@@ -18,42 +17,35 @@ if (chatRoom) {
     },
 
     received(data) {
-    
-      const messageElement = document.createElement('div');
-      if(userId === data.id)
-      {
-        messageElement.innerHTML =
-       `<div class="m-1 p-1 ms-auto" style="background-color: white; border-radius: 10px;max-width:250px;">
+      const messageElement = document.createElement("div");
+      if (userId === data.id) {
+        messageElement.innerHTML = `<div class="m-1 p-1 ms-auto" style="background-color: white; border-radius: 10px;max-width:250px;">
         <p>
         <strong class='text-success'>${data.name}</strong>:${data.message}
         </p>
         </div>`;
-      }
-      else
-      {
-        messageElement.innerHTML=
-      ` <div class="m-1 p-1" style="background-color: white; border-radius: 10px;max-width:250px;">
+      } else {
+        messageElement.innerHTML = ` <div class="m-1 p-1" style="background-color: white; border-radius: 10px;max-width:250px;">
        <p>
        <strong class='text-warning'>${data.name}</strong>:${data.message}
        </p>
-       </div>`
+       </div>`;
       }
 
-     chatMessages.appendChild(messageElement);
-    }
+      chatMessages.appendChild(messageElement);
+    },
   });
 
+  chatForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const message = document.getElementById("chat-text-input1").value;
 
-chatForm.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const message = document.getElementById('chat-text-input1').value;
+    consumer.subscriptions.subscriptions[0].perform("send_message", {
+      message: message,
+      id: userId,
+    });
+    console.log("Sending message:", message);
 
-  consumer.subscriptions.subscriptions[0].perform('send_message', { message: message,id: userId })
-  console.log("Sending message:", message);
-
-
-  document.getElementById('chat-text-input1').value = '';
-})
-
+    document.getElementById("chat-text-input1").value = "";
+  });
 }
-
