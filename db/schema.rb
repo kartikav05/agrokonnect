077467432647a709abcd740d3e3ea0af3d60fad9
kartikav05 <kartik@Kartik>
2023-09-19 22:26:48 +0530

    migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_145147) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_165317) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_145147) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.date "start"
+    t.date "end"
+    t.integer "user_id", null: false
+    t.string "req_mail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -104,6 +115,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_145147) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "events", "users"
   add_foreign_key "messages", "users"
   add_foreign_key "notifications", "users"
   add_foreign_key "tasks", "users"
