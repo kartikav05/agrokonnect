ca00b70465b7145bc1793422b5bee3d41b9ff3ef
kartikav05 <kartik@Kartik>
2023-09-27 18:36:10 +0530

    added some seed data to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Region.create([{ name: "Mandya" }, { name: "Mysore" },{ name: "Hubli" },{ name: "Dharwad" },{ name: "Chikkamagaluru" }])
+Region.create([{ name: "Belgavi" }, { name: "Bellary" },{ name: "Bidar" },{ name: "Chitradurga" },{ name: "Davangere" }])
