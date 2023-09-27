2e6fdd97936ff707f53dcdbd88c4ea3b88ee2a1d
kartikav05 <kartik@Kartik>
2023-09-27 16:14:53 +0530

    Added Region Model to the database and seeded some data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_145147) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_103948) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,6 +39,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_145147) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.date "start"
+    t.date "end"
+    t.integer "user_id", null: false
+    t.string "req_mail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
@@ -59,6 +70,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_145147) do
     t.index ["task_id"], name: "index_notifications_on_task_id"
     t.index ["tool_id"], name: "index_notifications_on_tool_id"
     t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -104,6 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_145147) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "events", "users"
   add_foreign_key "messages", "users"
   add_foreign_key "notifications", "users"
   add_foreign_key "tasks", "users"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Region.create([{ name: "Mandya" }, { name: "Mysore" },{ name: "Hubli" },{ name: "Dharwad" },{ name: "Chikkamagaluru" }])
