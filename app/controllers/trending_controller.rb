75b70ac1adfcff31c08eb7d329d8f5fdec4ece58
kartikav05 <kartikav2001@gmail.com>
2023-09-29 17:21:08 +0530

    Added Controller and view file for the trending section

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -1,4 +1,8 @@
 class TrendingController < ApplicationController
   def index
+    sel_location = session[:my_re]
+    @news = Info.where(location: sel_location)
+    @tools = Tool.where(location: sel_location)
+    @tasks = Task.where(location: sel_location)
   end
 end
