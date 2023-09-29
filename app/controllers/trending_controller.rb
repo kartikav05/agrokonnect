accade96c5021ad554c44b8f31e5375d263942e9
kartikav05 <kartikav2001@gmail.com>
2023-09-29 18:40:12 +0530

    Made changes in the controller to authenticate user

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -1,4 +1,5 @@
 class TrendingController < ApplicationController
+  before_action :authenticate_user!
   def index
     sel_location = session[:my_re]
     @news = Info.where(location: sel_location)
@@ -12,6 +13,7 @@ class TrendingController < ApplicationController
       @tool_usernames[tool.id] = user.name if user
     end
 
+    @task_usernames = {}
     @tasks.each do |task|
       user = User.find_by(id: task.user_id)
       @task_usernames[task.id] = user.name if user
