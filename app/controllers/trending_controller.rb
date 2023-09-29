b302861461045beb6d3789bb0a3c6218b074e708
kartikav05 <kartikav2001@gmail.com>
2023-09-29 17:47:05 +0530

    Made changes in the controller to find the tool and task owner

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -4,5 +4,18 @@ class TrendingController < ApplicationController
     @news = Info.where(location: sel_location)
     @tools = Tool.where(location: sel_location)
     @tasks = Task.where(location: sel_location)
+    @notification = Notification.new
+
+    @tool_usernames = {}
+    @tools.each do |tool|
+      user = User.find_by(id: tool.user_id)
+      @tool_usernames[tool.id] = user.name if user
+    end
+
+    @tasks.each do |task|
+      user = User.find_by(id: task.user_id)
+      @task_usernames[task.id] = user.name if user
+    end 
+
   end
 end
