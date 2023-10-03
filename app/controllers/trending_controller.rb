f8a0386525b08785c623ca86fbdd87feb26e687e
kartikav05 <kartikav2001@gmail.com>
2023-10-03 18:44:47 +0530

    Added crops and seed data in trending section and made changes in the controller for to fetch the data

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -1,10 +1,15 @@
 class TrendingController < ApplicationController
   before_action :authenticate_user!
   def index
+
     sel_location = session[:my_re]
+    
     @news = Info.where(location: sel_location)
     @tools = Tool.where(location: sel_location)
     @tasks = Task.where(location: sel_location)
+    @seeds = Seed.where(location: sel_location)
+    @crops = Crop.where(location: sel_location)
+
     @notification = Notification.new
 
     @tool_usernames = {}
@@ -18,6 +23,18 @@ class TrendingController < ApplicationController
       user = User.find_by(id: task.user_id)
       @task_usernames[task.id] = user.name if user
     end 
+
+    @seeds_usernames ={}
+    @seeds.each do |seed|
+      user = User.find_by(id: seed.user_id)
+      @seeds_usernames[seed.id] = user.name if user
+    end
+
+    @crops_usernames ={}
+    @crops.each do |crop|
+      user = User.find_by(id: crop.user_id)
+      @crops_usernames[crop.id] = user.name if user
+    end
 
   end
 end
